ab9b1a133c32a399519fffd6af167c1a85349c98
Komatsubara Masato <m-koma@nifty.com>
2022-05-20 10:56:06 +0900

    テキストボックスのみを追加

--- update_produce_list.py
+++ update_produce_list.py
@@ -5,5 +5,12 @@ root = tk.Tk()
 root.title('農産物価格更新')
 root.geometry('450x650')
 
+# テキストボックスの作成
+text_box = tk.Entry(root)
+text_box.place(x=50, y=50, width=185, height=40)
+
+text_box2 = tk.Entry(root)
+text_box2.place(x=260, y=50, width=140, height=40)
+
 # アプリの実行
 root.mainloop()
